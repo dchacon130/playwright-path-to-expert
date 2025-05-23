83f1cdc7c24372bfaa58ec310717dd813f617dd1
Diego Chacon <dchacon130@gmail.com>
2025-05-22 19:51:05 -0500

    Estructura basica de un escript

---
+++ tests/browser.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Estructura basica de un script de Playwright', () => {
+
+    test.beforeAll(async () => {
+
+    });
+
+    test.beforeEach(async ({ page }) => {
+
+    });
+
+    test('Este seria el primer test', async ({ page }) => {
+
+    });
+
+    test.afterEach(async ({ page }) => {
+
+    });
+
+    test.beforeAll(async () => {
+
+    });
+});
