cd2283f76467a27c58658c1d5b8e006b48283b9d
Diego Chacon <dchacon130@gmail.com>
2025-05-23 11:48:13 -0500

    Adding context 1 and context 2 to new page or newContext browser

--- tests/browser.spec.js
+++ tests/browser.spec.js
@@ -1,11 +1,28 @@
-import { test, expect, chromium } from '@playwright/test';
+import { test, expect, chromium, webkit } from '@playwright/test';
 
 test.describe('Estructura basica de un script de Playwright', () => {
     let browser;
     let page;
+    let page1;
+    let context1;
+    let page2;
+    let context2;
 
     test.beforeAll(async () => {
-        browser = await chromium.launch({ headless: false });
+        browser = await webkit.launch({ headless: false });
+        console.log('--> Navegador webkit creado');
+        
+        context1 = await browser.newContext();
+        page1 = await context1.newPage();
+
+        context2 = await browser.newContext();
+        page2 = await context1.newPage();
+        context2.storageState();
+        context2.setGeolocation();
+        context2.grantPermissions();
+
+        console.log(`BrowserContext: ${browser.contexts()}`);
+        console.log(`Is Connected: ${browser.isConnected()}`);
         console.log('--> Navegador creado');
     });
 
@@ -30,6 +47,12 @@ test.describe('Estructura basica de un script de Playwright', () => {
         if (browser) {
             await browser.close();
             console.log('--> Navegador cerrado.')
+        }
+        else if(context1){
+            context1.close();
+        }
+        else if(context2){
+            context2.close();
         }
     });
 });
