14890f2ebedefc604a3e55703734b0a6797cfda1
Diego Chacon <dchacon130@gmail.com>
2025-06-12 17:11:45 -0500

    adding elements to login

--- tests/elements.spec.js
+++ tests/elements.spec.js
@@ -1,6 +1,24 @@
 import { chromium, test } from '@playwright/test';
-import { beforeEach } from 'node:test';
+import { afterEach, beforeEach } from 'node:test';
 
-beforeEach(async () => {
+test.beforeEach(async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    console.log('Soy before each de Elements');
+});
+
+test.describe('Locators en Playwright', () => {
+    
+    test('Login en Orange HRM live', async ( {page} )=> {
+        console.log('Login en Orange HRM live...');
+        await page.locator('input[name="username"]').fill('Admin');
+        await page.getByPlaceholder('Password').fill('admin123');
+        await page.getByRole('button', { name: 'Login' }).click();
+    });
     
 });
+
+test.afterEach(async ({page}) => {
+    await page.close();
+    console.log("Soy before each de Elements");
+});
+
