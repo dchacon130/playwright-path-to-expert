e025c96c29f1b2f4868658c716ea8aa32cbf4ffe
Diego Chacon <dchacon130@gmail.com>
2025-06-16 08:24:09 -0500

    waits and waitForRequest

--- tests/elements.spec.js
+++ tests/elements.spec.js
@@ -1,32 +1,51 @@
 import { expect, test } from '@playwright/test';
+import { request } from 'http';
 
 test.beforeEach(async ({page}) => {
+    const requestPromise = page.waitForRequest('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    const request = await requestPromise;
+    console.log('Petición interceptada: ', request.url);
+    console.log('Método: ', request.method());
 });
 
 test.describe('Locators en Playwright', () => {
     
     test('Login en Orange HRM live', async ( {page} )=> {
-        
+        //waits
         const btnLogin = page.getByRole('button', { name: 'Login' });
         const dashboard = page.getByRole('heading', { name: 'Dashboard'});
         const search = page.getByPlaceholder('Search');
         const admin = page.getByText('Admin');
         const lblSystemUsers = page.getByRole('heading', { name: 'System Users'});
+        const addbutton = page.getByRole('button', { name: 'Add' });
 
         console.log('Login en Orange HRM live...');
         await page.locator('input[name="username"]').fill('Admin');
         await page.getByPlaceholder('Password').fill('admin123');
+        const [loginResponse, dashboardResponse] = await Promise.all([
+            page.waitForResponse('**/auth/validate'),
+            page.waitForResponse(resposne => 
+                resposne.url().includes('/dashboard') || 
+                resposne.url().includes('/api/v2/dashboard')
+            ),
+            await btnLogin.click()
+        ]);
+
+        console.log('Login status: ', loginResponse.status());
+        console.log('Dashboard loaded: ', dashboardResponse.status());
+        
+        
         await page.screenshot({ path: 'screenshots/despues-login.png', fullPage: true });
-        await btnLogin.click();
-        
-        
         //assertion login
         await expect(dashboard).toBeVisible();
         await expect(dashboard).toHaveText('Dashboard');
         await expect(btnLogin).not.toBeVisible();
         
         try{
+            //waits
+            await page.waitForLoadState('networkidle');
             await search.waitFor({ state: 'visible', timeout: 10000 });
             
             const searchExist = await search.count() > 0;
@@ -36,10 +55,9 @@ test.describe('Locators en Playwright', () => {
             if (searchExist && isVisible && isEnabled){
                 await search.fill('Admin');
                 await admin.click();
-                console.log('Campo de busqueda entrando a Admin');
                 
                 //assertion Admin
-                await expect(page.getByRole('button', { name: 'Add' })).toBeVisible();
+                await expect(addbutton).toBeVisible();
                 //await expect(lblSystemUsers).toHaveValue('System Users');
                 await lblSystemUsers.screenshot({ path: 'screenshots/lblSystemUser.png' });
             }else {
