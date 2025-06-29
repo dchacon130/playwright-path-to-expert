3ac534bf5d1e8eff2b07c5ba5b25ca6765480f95
Diego Chacon <dchacon130@gmail.com>
2025-06-29 17:46:26 -0500

    Solving problems in actions

--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('El usuario puede ver la pagina de inventario', async ({page}) => {
     await page.goto('https://www.saucedemo.com/inventory.html');
 
-    await expect(page.locator('.inventory_list')).toBeVisible();
+    await expect(page.locator('.inventory_container')).toBeVisible();
     await expect(page.getByText('Products')).toBeVisible();
 });
